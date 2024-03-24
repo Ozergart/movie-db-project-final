8ef95671b6175221a741eff3d21435edfa3ebb45
Kiril <ozergart@gmail.com>
2024-03-24 14:49:38 +0200

    serviceRework

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,13 +2,11 @@ import axios from "axios";
 import {baseURL} from "../constants";
 
 const apiService = axios.create({baseURL})
-const apiServiceFetch=(url:string,params:{}) => fetch(url,params)
 apiService.interceptors.request.use((request)=>{
     request.headers.Authorization = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMjc0NDJkZDg1NmU0MzliMjk2NDlkMjE5NDFkNmFmZiIsInN1YiI6IjY1ZDlkOWUxZTc4ZTJkMDE4NjgwZTRkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BR4Phoh54qV9MDfUL9tForE69wPOmMj4La5d_VUunoU'
     return request
 })
 
 export {
-    apiService,
-    apiServiceFetch
+    apiService
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,4 @@
-import {apiService, apiServiceFetch} from "./apiService";
+import {apiService} from "./apiService";
 import {urls} from "../constants";
 import {IRes} from "../types";
 import {IUser} from "../interfaces";
@@ -6,33 +6,18 @@ import {IUser} from "../interfaces";
 
 const userService = {
     get: (): IRes<IUser> => apiService.get(`${urls.user.base}/21028568`),
-    setFavorite: (userId: number, movieId: number, status: boolean) => apiServiceFetch("https://api.themoviedb.org/3" + urls.movies.favorite(userId), {
-        method: 'POST',
-        headers: {
-            accept: 'application/json',
-            'content-type': 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMjc0NDJkZDg1NmU0MzliMjk2NDlkMjE5NDFkNmFmZiIsInN1YiI6IjY1ZDlkOWUxZTc4ZTJkMDE4NjgwZTRkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BR4Phoh54qV9MDfUL9tForE69wPOmMj4La5d_VUunoU'
-        },
-        body: JSON.stringify({
+    setFavorite: (userId: number, movieId: number, status: boolean) => apiService.post(urls.movies.favorite(userId), {
             media_type: "movie",
             media_id: movieId,
             favorite: status
-        })
-    }),
-    setWatchList: (userId: number, movieId: number, status: boolean) => apiServiceFetch("https://api.themoviedb.org/3" + urls.movies.watchList(userId), {
-        method: 'POST',
-        headers: {
-            accept: 'application/json',
-            'content-type': 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMjc0NDJkZDg1NmU0MzliMjk2NDlkMjE5NDFkNmFmZiIsInN1YiI6IjY1ZDlkOWUxZTc4ZTJkMDE4NjgwZTRkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BR4Phoh54qV9MDfUL9tForE69wPOmMj4La5d_VUunoU'
-        },
-        body: JSON.stringify({
+        }
+    ),
+    setWatchList: (userId: number, movieId: number, status: boolean) => apiService.post(urls.movies.watchList(userId), {
             media_type: "movie",
             media_id: movieId,
             watchlist: status
-        })
-    }),
-}
+        }
+    )}
 export {
     userService
 }
